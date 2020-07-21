e5609c713c4c9c447b30c41284ee4781ea423d1b
Martin David Valentino <martin.valentino@cultureamp.com>
2020-07-21 21:35:30 +1000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+let idCount = links.length;
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (parent, args) => {
+      return links.filter((link) => link.id === args.id)[0];
+    },
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      const link = links.filter((link) => link.id === args.id)[0];
+      if (link) {
+        link.url = args.url;
+        link.description = args.description;
+      }
+      return link;
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
